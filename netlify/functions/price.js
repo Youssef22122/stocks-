4269f5c695fa4a641e10e9621abd6be1689cee15
Youssef22122 <ym3lnahas@gmail.com>
2025-04-14 23:10:49 +0100

    Add files via upload

---
+++ netlify/functions/price.js
@@ -0,0 +1,24 @@
+
+import fetch from 'node-fetch';
+
+export async function handler(event, context) {
+  const symbol = event.queryStringParameters.symbol;
+  if (!symbol) return { statusCode: 400, body: "Missing symbol" };
+
+  const API_KEY = "AK8YD647KME4FBKKX47I";
+  const SECRET_KEY = "etbpfBCfTBTbs1AVlqmF7x1hBXnc8ikvyY5r0iMh";
+  const url = `https://data.alpaca.markets/v2/stocks/${symbol}/quotes/latest`;
+
+  try {
+    const res = await fetch(url, {
+      headers: {
+        'APCA-API-KEY-ID': API_KEY,
+        'APCA-API-SECRET-KEY': SECRET_KEY
+      }
+    });
+    const data = await res.json();
+    return { statusCode: 200, body: JSON.stringify({ price: data.quote.ap }) };
+  } catch (err) {
+    return { statusCode: 500, body: JSON.stringify({ error: err.message }) };
+  }
+}
